ebca1acc99959dc9903cff8eb114678799c72076
ninja-dev917 <webdev917@outlook.com>
2021-11-24 19:22:50 -0600

    add the tasks mapping into the todolist smart contract

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,22 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
-  uint taskCount = 0;
+  uint public taskCount = 0;
+
+  struct Task{
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+
+  constructor() public {
+    createTask("Check out the ninja-dev917 in github");
+  }
 }
